9e69f64f6405ac2cd8363d20d30e5b16fed5e5ce
RosieOh <20172207@gm.hannam.ac.kr>
2026-06-20 20:24:01 +0900

    CHORE: 애플리케이션 공통 상수 패키지 초기화 파일을 추가한다.

---
+++ app/constants/__init__.py
@@ -0,0 +1,13 @@
+"""Application-wide constants."""
+
+from app.constants.audio import AudioConstants
+from app.constants.fds import FdsConstants
+from app.constants.triton import TritonConstants
+from app.constants.websocket import WebSocketConstants
+
+__all__ = [
+    "AudioConstants",
+    "FdsConstants",
+    "TritonConstants",
+    "WebSocketConstants",
+]
